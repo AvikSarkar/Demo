bdae58816f44f241c59ef75602a1f2a404d1632d
Avik Kumar Sarkar <sarkarA@advisory.com>
2014-03-24 02:17:53 +0530

    Another Commit to index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GitDemo.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application 1.";
 
             return View();
         }
